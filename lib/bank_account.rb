f99cc87a7eba38b54631f1ae56e0b33b08b2f8c2
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-07 03:10:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,5 @@
 class BankAccount
+<<<<<<< HEAD
 
   attr_accessor :balance, :status
   attr_reader :name
@@ -27,5 +28,30 @@ class BankAccount
 
   def close_account
     self.status = "closed"
+=======
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(account_name)
+    @name = account_name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+>>>>>>> 5244764df72f0fcfdd794526f3d7d02c7ff8aee2
   end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Transfer
  attr_accessor :sender, :receiver, :amount, :status
 
@@ -31,6 +32,38 @@ class Transfer
     else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."      
+=======
+require 'pry'
+class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+    else
+      @status = "rejected"
+    end
+    return "Transaction rejected. Please check your account balance."
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+>>>>>>> 5244764df72f0fcfdd794526f3d7d02c7ff8aee2
     end
   end
 end
