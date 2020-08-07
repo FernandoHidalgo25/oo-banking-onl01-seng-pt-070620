5e8f2359f81d740e0d013deb073f89b4817a79c0
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-07 03:11:58 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,4 @@
 class BankAccount
-<<<<<<< HEAD
 
   attr_accessor :balance, :status
   attr_reader :name
@@ -28,30 +27,4 @@ class BankAccount
 
   def close_account
     self.status = "closed"
-=======
-  attr_accessor :balance, :status
-  attr_reader :name
-
-  def initialize(account_name)
-    @name = account_name
-    @balance = 1000
-    @status = "open"
-  end
-
-  def deposit(money)
-    @balance += money
-  end
-
-  def display_balance
-    "Your balance is $#{@balance}."
-  end
-
-  def valid?
-    @status == "open" && @balance > 0 ? true : false
-  end
-
-  def close_account
-    @status = "closed"
->>>>>>> 5244764df72f0fcfdd794526f3d7d02c7ff8aee2
-  end
 end
